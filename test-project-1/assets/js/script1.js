157ed9e71c06a978a7efcab5eeaf840ee8760f38
Nina Jade <jaderiver64@gmail.com>
2021-01-27 19:33:47 -0600

    updates

--- test-project-1/assets/js/script1.js
+++ test-project-1/assets/js/script1.js
@@ -1,18 +1,16 @@
-// Cookie settings for cross-site access
-document.cookie = 'cookie1=value1; SameSite=Lax';
-document.cookie = 'cookie2=value2; SameSite=None; Secure';
 // https://api.airvisual.com/v2/nearest_city?key=40f410cd-9102-4b7b-9aed-cdbbce23a985
 // 40f410cd-9102-4b7b-9aed-cdbbce23a985
-var cityArray = [];
-// Fetch the API data for current Air Quality
+
+var aqiArray = [];
+
+
 var getCurrentAirInfo = function() {
     var apiUrl = "https://api.airvisual.com/v2/nearest_city?key=40f410cd-9102-4b7b-9aed-cdbbce23a985";
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(dataResult) {
                 console.log(dataResult);
-                console.log(dataResult.data.location.coordinates);
-                displayAQIInformation(dataResult);
+                createAPIObject(dataResult);
 
             });
         } else {
@@ -22,102 +20,50 @@ var getCurrentAirInfo = function() {
 };
 
 
-function searchButtonHandler(input) {
-    console.log(input);
-    var apiUrl = "https://api.opencagedata.com/geocode/v1/json?q=" +
-        input + "&key=974cf3d56a9f45d58e79a7ec8b1f7842";
-    // Geocode API - Open cage Data
-    fetch(apiUrl).then(function(response) {
+// searchAQIResult is the function to call from script-search taking in latitude and longitude parameters
+
+var searchAQIResult = function(lat, lng) {
+
+    var url = "http://api.airvisual.com/v2/nearest_city?lat=" +
+        lat + "&lon=" + lng + "&key=40f410cd-9102-4b7b-9aed-cdbbce23a985";
+
+    fetch(url).then(function(response) {
+
         if (response.ok) {
-            response.json().then(function(geoData) {
-                console.log(geoData)
-                    //results[0].geometry.lat, results[0].geometry.lng
-                var url = "http://api.airvisual.com/v2/nearest_city?lat=" +
-                    geoData.results[0].geometry.lat + "&lon=" + geoData.results[0].geometry.lng + "&key=40f410cd-9102-4b7b-9aed-cdbbce23a985";
-                searchResult(url);
+            response.json().then(function(thisData) {
+                console.log(thisData);
+                createAPIObject(thisData);
+                console.log(aqiArray);
+
             });
         } else {
             alert("Error: " + response.statusText);
-            // Check for problems
+
         }
     });
-};
+}
 
-var searchResult = function(url) {
-        /*
-                fetch(url).then(function(response) {
-
-                    if (response.ok) {
-                        response.json().then(function(thisData) {
-                            console.log(thisData)
-                            var testingEl = document.getElementById("why");
-                            var aqiBadgeElement = createBadge(thisData.data.current.pollution.aqius);
-                            testingEl.innerHTML = "The AQI For Searched Region: " + aqiBadgeElement;
-                            var location = thisData.data.city + ", " + thisData.data.state + ", " + thisData.data.country;
-                            var myObj = {
-                                name: location,
-                                rating: "getRating()" // future concept of personal rating for risk assessment
-                            };
-                            cityArray.push(myObj);
-                            saveSearch();
-                        });
-                    } else {
-                        alert("Error: " + response.statusText);
-                        // Check for problems
-                    }
-                });
-        */
-    }
-    // Finds and saves the data needed from the API
-var displayAQIInformation = function(results) {
-
-    /* NOTE: This data is displayed this way (innerHTML) for testing purposes only - this way I can see my output.
-    the plan is to build information objects that can be saved and accessed as needed. */
+var createAPIObject = function(results) {
+    // This will work with HTML and CSS to display the variables
     var cityFormatted = results.data.city + ", " + results.data.state + ", " + results.data.country;
-    var headerEl = document.getElementById("current-conditions-header");
-    headerEl.innerHTML = "<div><h1>Ahoy, " + cityFormatted + "!</h1></div>";
-
-    var contentEl = document.getElementById("content");
     var iconIdEl = results.data.current.weather.ic;
     var link = "https://openweathermap.org/img/wn/" + iconIdEl + "@2x.png";
     var imgCode = "<img src='" + link + "' alt='icon'>";
-
-    var aqiBadgeElement = createBadge(results.data.current.pollution.aqius);
+    var aqi = results.data.current.pollution.aqius;
     var pollutantName = getPollutant(results);
+    var myObj = {
+        name: cityFormatted,
+        aqi: aqi,
+        pollutant: pollutantName,
+        img: imgCode
+    };
+    console.log(myObj);
+    aqiArray.push(myObj);
+    console.log(aqiArray);
 
-
-    contentEl.innerHTML = "<div>" + imgCode + "</div><br><h3>Temp: " +
-        convertToF(results.data.current.weather.tp) + "F" +
-        "<br><br><div class ='center-align'><div>Current AQI (US):</div><div> " + aqiBadgeElement + "</div></div>" +
-        "<br><div class='pollutants'>Primary Pollutant: " + pollutantName +
-        "</div><br></h3><h6>This is the AirVisual App at work.</h6></div>";
-    searchButtonHandler(cityFormatted); // This is just to show that the search function works,
-    // we will link up the search input button instead of this function.
 };
 
 
-// Creates a badge using materialize that indicates the AQI with color coding
-var createBadge = function(aqiValue) {
-    var badgeCode = "<h3><a class='btn-floating btn-large waves-effect waves-light";
-
-    if (aqiValue <= 50) {
-        badgeCode += "green";
-    } else if (aqiValue <= 100) {
-        badgeCode += "yellow";
-    } else if (aqiValue <= 150) {
-        badgeCode += "orange";
-    } else if (aqiValue <= 200) {
-        badgeCode += "red";
-    } else if (aqiValue <= 300) {
-        badgeCode += "violet";
-    } else if (aqiValue > 301) {
-        badgeCode += "maroon";
-    }
-    badgeCode += "'>" + aqiValue + "</a></h3>";
-    return badgeCode;
-};
-
-// The API uses Celsius - so we want to convert this to Fahrenheit
 
 function convertToF(celsius) {
     return Math.trunc(celsius * 9 / 5 + 32);
@@ -146,24 +92,28 @@ var getPollutant = function(result) {
 
     return pollutantName;
 };
+/* Test Code:
+ searchAQIResult(1, 2);
+getCurrentAirInfo();*/
+/*  This is potentially not needed, but keeping this working code in case we do
 
-function saveSearch() {
-    localStorage.setItem("cityArray", JSON.stringify(cityArray));
-    console.log("search recorded");
-    console.log(cityArray);
-};
+            function saveSearch() {
+                localStorage.setItem("cityArray", JSON.stringify(cityArray));
+                console.log("search recorded");
+                console.log(cityArray);
+            };
 
-function loadSearch() {
-    var savedSearches = JSON.parse(localStorage.getItem("cityArray"));
+            function loadSearch() {
+                var savedSearches = JSON.parse(localStorage.getItem("cityArray"));
 
-    if (savedSearches) {
-        cityArray = savedSearches;
-    } else { return false; }
-    console.log("Search History Found...");
+                if (savedSearches) {
+                    cityArray = savedSearches;
+                } else { return false; }
+                console.log("Search History Found...");
 
-    cityArray = JSON.parse(JSON.stringify(savedSearches));
-    console.log(cityArray);
-    /* createHistory(); - a future function to build the search history panel */
+                cityArray = JSON.parse(JSON.stringify(savedSearches));
+                console.log(cityArray);
+                /* createHistory(); - a future function to build the search history panel 
 };
 
 // Calls loadSearch() to load the saved entries up as the page loads
@@ -172,3 +122,32 @@ function loadSearch() {
 getCurrentAirInfo();
 
 loadSearch();
+
+
+/* Adjusting this area in the future to suit our needs - for now it's got the
+
+var createBadge = function(aqiValue) {
+    var badgeCode = "<h3><a class='btn-floating btn-large waves-effect waves-light";
+
+    if (aqiValue <= 50) {
+        badgeCode += "green";
+    } else if (aqiValue <= 100) {
+        badgeCode += "yellow";
+    } else if (aqiValue <= 150) {
+        badgeCode += "orange";
+    } else if (aqiValue <= 200) {
+        badgeCode += "red";
+    } else if (aqiValue <= 300) {
+        badgeCode += "violet";
+    } else if (aqiValue > 301) {
+        badgeCode += "maroon";
+    }
+    badgeCode += "'>" + aqiValue + "</a></h3>";
+    return badgeCode;
+};
+
+    <script src="assets\aqi-covid\script-covid.js"></script>
+    <script src="assets\aqi-covid\script-aqi.js"></script>
+ */
+
+// The API uses Celsius - so we want to convert this to Fahrenheit/
--- test-project-1/assets/js/script2.js
+++ test-project-1/assets/js/script2.js
@@ -2,21 +2,49 @@
 document.cookie = 'cookie1=value1; SameSite=Lax';
 document.cookie = 'cookie2=value2; SameSite=None; Secure';
 
-// So far worksd good!  Will get this using the fips
 // API KEY: https://api.covidactnow.org/v2/county/{{FIPS}}.json?apiKey=445bc14aef9b4a7798f42f69d834218d
-var attempt = function() {
-    // this works great, tons of data I know we can use.  Also a risk guide: https://globalepidemics.org/wp-content/uploads/2020/09/key_metrics_and_indicators_v5-1.pdf
-    var apiUrl = "https://api.covidactnow.org/v2/county/49017.json?apiKey=445bc14aef9b4a7798f42f69d834218d";
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(dataResult) {
-                console.log(dataResult);
+var covidArray = [];
 
-            });
-        } else {
-            alert("Error: " + response.statusText);
-        }
-    });
 
-}
-attempt();
+// Function getCovidData(fips) is meant to be called by script-search.js to get Covid Data from a FIPS 5-digit code parameter
+var getCovidData = function(fips) {
+        var currentCases;
+        var currentDeaths;
+        var currentRiskFactor;
+        var currentNewCases;
+
+        // Covid Risk Assessment Guide by Harvard: https://globalepidemics.org/wp-content/uploads/2020/09/key_metrics_and_indicators_v5-1.pdf
+
+        var apiUrl = "https://api.covidactnow.org/v2/county/" +
+            fips + ".json?apiKey=445bc14aef9b4a7798f42f69d834218d";
+        fetch(apiUrl).then(function(response) {
+            if (response.ok) {
+                response.json().then(function(dataResult) {
+                    console.log(dataResult);
+                    currentCases = dataResult.actuals.cases;
+                    // Cumulative number of confirmed or suspected cases
+                    currentDeaths = dataResult.actuals.deaths;
+                    // Cumulative number of deaths that are suspected or confirmed to have been caused by COVID-19
+                    currentRiskFactor = dataResult.riskLevels.overall;
+                    // Enum:{ 0 1 2 3 4 5 } Overall risk level for region.
+                    currentNewCases = dataResult.actuals.newCases;
+                    /* An integer or null (Newcases) 
+                    - New confirmed or suspected cases.
+                    - New cases are a processed timeseries of cases - summing new cases may not equal the cumulative case count.*/
+                });
+                var covidDataObject = {
+                    cases: currentCases,
+                    deaths: currentDeaths,
+                    riskFactor: currentRiskFactor,
+                    newCases: currentNewCases
+                };
+                covidArray.push(covidDataObject);
+                console.log(covidDataObject);
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        });
+
+    }
+    /* Test Code:
+    getCovidData(49017);*/
